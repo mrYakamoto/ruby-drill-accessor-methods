0d5d03f71f71a953dbf3a4edf35fb6464ef22a57
GregoryW & nil <pair+mryakamoto.nilthacker@devbootcamp.com>
2015-12-07 11:08:52 -0800

    Finish challenge

--- source/company_employee.rb
+++ source/company_employee.rb
@@ -1,54 +1,28 @@
 class Company
 
+  attr_accessor :company_name
+  attr_reader :employees
+
   def initialize(name)
     @company_name  = name
     @employees   = []
   end
 
-  def company_name
-    @company_name
-  end
-
-  def company_name=(new_name)
-    @company_name = new_name
-  end
-
   def add_employee(employee)
     @employees << employee
-  end
-
-  def employees
-    @employees
   end
 end
 
 
 class Employee
 
+  attr_accessor :name, :position
+  attr_reader :id
+
   def initialize(name, position)
     @name     = name
     @position = position
     @id       = rand(100_000_000).to_s.insert(2, '-').insert(6, '-')
-  end
-
-  def name
-    @name
-  end
-
-  def name=(new_name)
-    @name = new_name
-  end
-
-  def position
-    @position
-  end
-
-  def position=(new_position)
-    @position = new_position
-  end
-
-  def id
-    @id
   end
 end
 
--- source/company_employee_spec.rb
+++ source/company_employee_spec.rb
@@ -10,10 +10,56 @@ describe Employee do
       expect(shereef.name).to eq("Shereef Bishay")
     end
 
+    it 'should be changeable' do
+      expect(shereef.name = "bob").to eq("bob")
+    end
+  end
+  describe '#position' do
+    #Example test
+    it 'should be readable' do
+      expect(shereef.position).to eq("Master Of The Bootiverse")
+    end
+
+    it 'should be changeable' do
+      expect(shereef.position = "Janitor").to eq("Janitor")
+    end
+  end
+  describe '#id' do
+    #Example test
+    it 'should be readable' do
+      id = shereef.id.match(/\d{2}-\d{3}-\d{2}/)
+      expect(shereef.id).should be_truthy
+    end
   end
 end
 
 #Every method should be tested!
 describe Company do
-end
+  let(:company) { Company.new("Panda Express") }
+  let(:shereef) { Employee.new("Shereef Bishay", "Master Of The Bootiverse") }
 
+  describe '#name' do
+    #Example test
+    it 'should be readable' do
+      expect(company.company_name).to eq("Panda Express")
+    end
+
+    it 'should be changeable' do
+      expect(company.company_name = "Baskin Robbins").to eq("Baskin Robbins")
+    end
+  end
+
+  describe '#add_employee' do
+    #Example test
+    it 'should be able to add an employee' do
+      employees_array = company.add_employee(shereef)
+      expect(employees_array.include?(shereef)).to be true
+    end
+  end
+
+  describe '#employees' do
+    it 'should be able to add an employee' do
+      expect(company.employees).to be_a (Array)
+    end
+  end
+end
